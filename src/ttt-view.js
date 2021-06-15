8cfdb391b866926e52b2bea3c6c123427b854ddd
Samuel Sacco <SamLSacco@aol.com>
2021-06-15 14:56:53 -0400

    first push

--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,7 +1,18 @@
 class View {
   constructor(game, el) {}
 
-  setupBoard() {}
+  setupBoard() {
+    const t = document.createElement("ul");
+    for (let e = 0; e < 3; e++)
+
+    for (let r = 0; r < 3; r++){
+      s = document.createElement("li")
+      s.dataset.pos = JSON.stringify([e, r])
+      t.append(s);
+    }
+
+    this.el.append(t)
+  }
   
   bindEvents() {}
 
